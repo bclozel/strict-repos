0b6a4576bb7dbc4dc19f77b6968f2aa2eef6a7b0
Brian Clozel <bclozel@pivotal.io>
2018-09-10 10:58:11 +0200

    Repro project for strict repo config mode
    See https://github.com/spring-projects/spring-boot/issues/14328
    and https://jira.spring.io/browse/DATACMNS-1174

---
+++ src/main/java/com/example/strictrepos/StrictReposApplication.java
@@ -0,0 +1,12 @@
+package com.example.strictrepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrictReposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrictReposApplication.class, args);
+	}
+}
